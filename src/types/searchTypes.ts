2aeb8f47d66837d7761c7349910ce3576b60cf9d
mrenann <marcos.renann.br@gmail.com>
2025-01-14 06:45:19 -0300

    refactor: types

--- src/types/searchTypes.ts
+++ src/types/searchTypes.ts
@@ -1,19 +1,9 @@
-export interface ProfileImage {
-  small: string;
-  medium: string;
-  large: string;
-}
-
 export interface User {
   id: string;
   username: string;
   name: string;
   first_name: string;
   last_name: string;
-  instagram_username: string;
-  twitter_username: string;
-  portfolio_url: string;
-  profile_image: ProfileImage;
 }
 
 export interface ImageUrls {
@@ -26,21 +16,9 @@ export interface ImageUrls {
 
 export interface Image {
   id: string;
-  created_at: string;
-  width: number;
-  height: number;
-  color: string;
-  blur_hash: string;
-  likes: number;
-  liked_by_user: boolean;
   description: string;
   user: User;
   urls: ImageUrls;
-  links: {
-    self: string;
-    html: string;
-    download: string;
-  };
 }
 
 export interface SearchResponse {
