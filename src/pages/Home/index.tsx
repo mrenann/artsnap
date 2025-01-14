3d4ec9f6c3830db2259034d423a7b196f1b4eb52
mrenann <marcos.renann.br@gmail.com>
2025-01-13 23:30:09 -0300

    feat: api init

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import {Text, View} from 'react-native';
 
 export default function Home() {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
 }
